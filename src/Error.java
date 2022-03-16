37bc4fa8f3952f3e7ab6b168f1e965e4b10fa930
sushantsingh2804 <sushantsinghruhil@gmail.com>
2022-03-16 22:58:08 +0530

    first commit

---
+++ src/Error.java
@@ -0,0 +1,9 @@
+public class Error {
+    public static void main(String[] args) {
+        int a = 4;
+        float b = 3;
+        int c = (int) ((++a) + b);
+        System.out.println(c);
+
+    }
+}
